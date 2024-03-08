84b6db3b32ae6af1c648530f385da30dfb52cccb
Kéwan Bernier <mail@kewan.fr>
2024-03-08 15:47:36 +0100

    Adding dotenv-kotlin lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 
     implementation(libs.paho.mqtt)
     implementation(libs.paho.mqtt.service)
+    implementation(libs.dotenv.kotlin)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
